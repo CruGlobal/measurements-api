646d99f4c06ee4275b6a0e8191ccf4f6014d3a89
Spencer Oberstadt <soberstadt@gmail.com>
2016-02-02 16:43:02 -0500

    use standard model initialization

--- app/models/token_and_user.rb
+++ app/models/token_and_user.rb
@@ -1,21 +1,13 @@
 class TokenAndUser < ActiveModelSerializers::Model
   attr_accessor :access_token, :person
 
-  def initialize(access_token, person)
-    @access_token = access_token
-    @person = person
-  end
-
   def assignments
     []
   end
 
   def attributes
-    {
-      access_token: @access_token,
-      person: @person,
-      status: 'success',
-      session_ticket: @access_token.attributes[:token]
-    }
+    super.merge(status: 'success',
+                session_ticket: @access_token.attributes[:token],
+                assignments: assignments)
   end
 end
