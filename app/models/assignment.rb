20c97293b116376a7e2b18caacbff6c7a4bd10a5
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-02-05 15:16:52 -0500

    Fixed role enum hash

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,4 @@
 class Assignment < ActiveRecord::Base
-  enum role: [blocked: 0, former_member: 1, self_assigned: 2, member: 3,
-              inherited_admin: 4, admin: 5, inherited_leader: 6, leader: 7]
+  enum role: { blocked: 0, former_member: 1, self_assigned: 2, member: 3,
+               inherited_admin: 4, admin: 5, inherited_leader: 6, leader: 7 }
 end
