83db4794a3e51fa3feee7a51020d78a10e3e2b87
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-02-10 13:38:35 -0500

    Stubbed out assignments controller

--- app/controllers/v5/assignments_controller.rb
+++ app/controllers/v5/assignments_controller.rb
@@ -3,15 +3,75 @@ module V5
     include CruLib::AccessTokenProtectedConcern
 
     def index
+      load_assignments
+      render_assignments
     end
 
     def show
+      load_assignment
+      render_assignment
     end
 
     def create
+      permit_params %i(username key_guid person_id ministry_id team_role)
+      build_assignment
+      if save_assignment
+        render_assignment :created
+      else
+        render_errors
+      end
     end
 
     def update
+      permit_params %i(team_role)
+      load_assignment
+      build_assignment
+      if save_assignment
+        render_assignment :ok
+      else
+        render_errors
+      end
+    end
+
+    private
+
+    def load_assignments
+      @assignments ||= current_user.assignments
+    end
+
+    def load_assignment
+      @assignment ||= Assignment.find_by(assignment_id: params[:id])
+    end
+
+    def build_assignment
+      @assignment ||= Assignment::NewUserAssignment.new
+      @assignment.attributes = assignment_params
+    end
+
+    def save_assignment
+      @assignment.save
+    end
+
+    def render_assignment(status = nil)
+      status ||= :ok
+      render json: @assignment, status: status
+    end
+
+    def render_assignments
+      render json: @assignments, status: :ok
+    end
+
+    def render_errors
+      render json: @assignment.errors.messages, status: :bad_request
+    end
+
+    def permit_params(params = {})
+      @permitted_params = params
+    end
+
+    def assignment_params
+      @permitted_params ||= {}
+      post_params.permit(@permitted_params)
     end
   end
 end
