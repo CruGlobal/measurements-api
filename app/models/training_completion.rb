c141c1759850f0cbb8e5d5474c1cafe30fb0a517
Spencer Oberstadt <soberstadt@gmail.com>
2016-02-23 11:09:52 -0500

    add TrainingCompletion validation

--- app/models/training_completion.rb
+++ app/models/training_completion.rb
@@ -1,4 +1,8 @@
 class TrainingCompletion < ActiveRecord::Base
   belongs_to :training
   validates :training, presence: { message: "Could not find required field: 'training_id'" }
+  validates :phase, presence: { message: "Could not find required field: 'phase'" }
+  validates :number_completed, presence: { message: "Could not find required field: 'number_completed'" },
+                               numericality: { greater_than: 0, message: 'cannot be negative' }
+  validates :date, presence: { message: "Could not find required field: 'date'" }
 end
