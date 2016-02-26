9eecc891f73593e21d677a4d2719739e04ecccb9
Spencer Oberstadt <soberstadt@gmail.com>
2016-02-26 15:10:41 -0500

    speed up ministry spec

--- spec/models/ministry_spec.rb
+++ spec/models/ministry_spec.rb
@@ -2,26 +2,31 @@ require 'rails_helper'
 
 describe Ministry, type: :model do
   describe 'scope: inherited_ministries(person)' do
-    let(:person) { FactoryGirl.create(:person) }
-    let!(:ministries) do
-      ministries = {}
-      ministries[:a] = FactoryGirl.create(:ministry, name: 'A')
-      ministries[:b] = FactoryGirl.create(:ministry, name: 'B')
-      ministries[:c] = FactoryGirl.create(:ministry, name: 'C')
-      ministries[:a1] = FactoryGirl.create(:ministry, name: 'A1', parent: ministries[:a])
-      ministries[:a2] = FactoryGirl.create(:ministry, name: 'A2', parent: ministries[:a])
-      ministries[:a3] = FactoryGirl.create(:ministry, name: 'A3', parent: ministries[:a])
-      ministries[:a21] = FactoryGirl.create(:ministry, name: 'A21', parent: ministries[:a2])
-      ministries[:a22] = FactoryGirl.create(:ministry, name: 'A22', parent: ministries[:a2])
-      ministries[:a31] = FactoryGirl.create(:ministry, name: 'A31', parent: ministries[:a3])
-      ministries[:c1] = FactoryGirl.create(:ministry, name: 'C1', parent: ministries[:c])
-      ministries[:c11] = FactoryGirl.create(:ministry, name: 'C11', parent: ministries[:c1])
-      ministries[:c12] = FactoryGirl.create(:ministry, name: 'C12', parent: ministries[:c1])
-      ministries[:c121] = FactoryGirl.create(:ministry, name: 'C121', parent: ministries[:c12])
-      ministries[:c122] = FactoryGirl.create(:ministry, name: 'C122', parent: ministries[:c12])
-      ministries[:c123] = FactoryGirl.create(:ministry, name: 'C123', parent: ministries[:c12])
-      ministries
+    before :all do
+      @ministries = {}
+      @ministries[:a] = FactoryGirl.create(:ministry, name: 'A')
+      @ministries[:b] = FactoryGirl.create(:ministry, name: 'B')
+      @ministries[:c] = FactoryGirl.create(:ministry, name: 'C')
+      @ministries[:a1] = FactoryGirl.create(:ministry, name: 'A1', parent: @ministries[:a])
+      @ministries[:a2] = FactoryGirl.create(:ministry, name: 'A2', parent: @ministries[:a])
+      @ministries[:a3] = FactoryGirl.create(:ministry, name: 'A3', parent: @ministries[:a])
+      @ministries[:a21] = FactoryGirl.create(:ministry, name: 'A21', parent: @ministries[:a2])
+      @ministries[:a22] = FactoryGirl.create(:ministry, name: 'A22', parent: @ministries[:a2])
+      @ministries[:a31] = FactoryGirl.create(:ministry, name: 'A31', parent: @ministries[:a3])
+      @ministries[:c1] = FactoryGirl.create(:ministry, name: 'C1', parent: @ministries[:c])
+      @ministries[:c11] = FactoryGirl.create(:ministry, name: 'C11', parent: @ministries[:c1])
+      @ministries[:c12] = FactoryGirl.create(:ministry, name: 'C12', parent: @ministries[:c1])
+      @ministries[:c121] = FactoryGirl.create(:ministry, name: 'C121', parent: @ministries[:c12])
+      @ministries[:c122] = FactoryGirl.create(:ministry, name: 'C122', parent: @ministries[:c12])
+      @ministries[:c123] = FactoryGirl.create(:ministry, name: 'C123', parent: @ministries[:c12])
     end
+
+    after :all do
+      Ministry.delete_all
+    end
+
+    let(:person) { FactoryGirl.create(:person) }
+    let!(:ministries) { @ministries }
     subject { Ministry.inherited_ministries(person).to_a }
 
     context 'person with no assignments' do
