2816a8ec0e230e5af703d026db525d127fbf45e0
Spencer Oberstadt <soberstadt@gmail.com>
2016-03-04 14:00:51 -0500

    add some helper methods to Measurement

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -6,22 +6,41 @@ class Measurement < ActiveRecord::Base
   validates :local_id, presence: true
   validates :total_id, presence: true
 
+  attr_accessor :total, :local, :person
+
+  def initialize(attributes = nil, options = {})
+    super(attributes, options)
+    @translations = {}
+  end
+
   def perm_link_stub
     perm_link.sub('lmi_total_custom_', '').sub('lmi_total_', '')
   end
 
   def localized_name(language, ministry)
     ministry = ministry.id if ministry.is_a? Ministry
-    translation = measurement_translations.find_by(language: language, ministry: ministry)
+    translation = translation_for(language, ministry)
     return english unless translation
     translation.name
   end
 
   def localized_description(language, ministry)
     ministry = ministry.id if ministry.is_a? Ministry
-    translation = measurement_translations.find_by(language: language, ministry: ministry)
+    translation = translation_for(language, ministry)
     return description unless translation
     translation.description
+  end
+
+  def locale(language, ministry_id)
+    return language if measurement_translations.where(language: language, ministry_id: ministry_id).any?
+    'en'
+  end
+
+  def translation_for(language, ministry_id)
+    return unless language && ministry_id
+    key = "#{language}-#{ministry_id}"
+    return @translations[key] if @translations[key].present?
+    @translations[key] = measurement_translations.find_by(language: language, ministry_id: ministry_id)
   end
 
   def self.find_by_perm_link(perm_link)
