a0ebbebd41a0c32f60786a99a31c99799e42aa87
draffensperger <d.raffensperger@gmail.com>
2016-03-22 15:58:17 -0400

    Fix ENTITY_NAMES constant

--- app/models/gr_sync/subscribed_entities.rb
+++ app/models/gr_sync/subscribed_entities.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 module GrSync
   class SubscribedEntities
+    # We don't subscribe to changes from Assignment to prevent another system
+    # from adding an Assignment entity and thus gaining access to data in the
+    # measurements api.
+    ENTITY_MODELS = [::Ministry, ::Person].freeze
+
+    NAMES_TO_MODELS = Hash[ENTITY_MODELS.map { |m| [m.entity_type, m] }].freeze
+    ENTITY_NAMES = NAMES_TO_MODELS.keys.freeze
+
     class << self
-      # We don't subscribe to changes from Assignment to prevent another system
-      # from adding an Assignment entity and thus gaining access to data in the
-      # measurements api.
-      ENTITY_MODELS = [::Ministry, ::Person].freeze
-
-      NAMES_TO_MODELS = Hash[ENTITY_MODELS.map { |m| [m.entity_type, m] }].freeze
-      ENTITY_NAMES = NAMES_TO_MODELS.values.freeze
-
       def entity_type_ids
         EntityTypeFinder.entity_type_ids(ENTITY_NAMES)
       end
