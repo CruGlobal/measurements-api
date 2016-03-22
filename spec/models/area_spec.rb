36e55bd233d9c93c432256478383a78460e9005a
draffensperger <d.raffensperger@gmail.com>
2016-03-22 10:31:48 -0400

    Implemented Area.for_gr_id

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -7,7 +7,7 @@ describe Area do
       expect(Area.for_code('AAOP')).to eq area
     end
 
-    it 'queries the global registry and creates a new are if one does not exist' do
+    it 'queries global registry and creates a new are if one does not exist' do
       gr_id = SecureRandom.uuid
       url = "#{ENV['GLOBAL_REGISTRY_URL']}/entities?entity_type=area&"\
         "filters[area_code]=EUWE"
@@ -35,6 +35,27 @@ describe Area do
         area = create(:area, gr_id: gr_id)
 
         expect(Area.for_gr_id(gr_id)).to eq area
+      end
+
+      it 'queries global registry and creates a new are if one does not exist' do
+        gr_id = SecureRandom.uuid
+        url = "#{ENV['GLOBAL_REGISTRY_URL']}/entities/#{gr_id}"
+        stub_request(:get, url).to_return(body: {
+          entity: {
+            area: {
+              id: gr_id, area_code: 'EUWE', is_active: true,
+              area_name: 'Western Europe'
+            }
+          }
+        }.to_json)
+
+        area = Area.for_gr_id(gr_id)
+
+        expect(area).to_not be_new_record
+        expect(area.gr_id).to eq gr_id
+        expect(area.code).to eq 'EUWE'
+        expect(area.name).to eq 'Western Europe'
+        expect(area).to be_active
       end
     end
   end
