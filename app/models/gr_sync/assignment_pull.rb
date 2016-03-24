dde5854615350f938660f3d0acbee23ad8395463
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-03-24 15:27:40 -0400

    Do not sync assignments not owned by us or assignments missing a team_role

--- app/models/gr_sync/assignment_pull.rb
+++ app/models/gr_sync/assignment_pull.rb
@@ -7,6 +7,8 @@ module GrSync
     end
 
     def sync
+      return unless @relationship['created_by'] == ENV.fetch('GLOBAL_REGISTRY_SYSTEM_ID')
+      return unless @relationship.key?('team_role')
       assignment_gr_id = @relationship['relationship_entity_id']
       assignment = Assignment.find_or_initialize_by(gr_id: assignment_gr_id)
       assignment.update!(
