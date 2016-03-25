0274765245a6de129ad30e2cd8e622dc778884c4
draffensperger <d.raffensperger@gmail.com>
2016-03-25 12:10:03 -0400

    Fix notifications controller spec and behavior

--- app/controllers/gr_sync/notifications_controller.rb
+++ app/controllers/gr_sync/notifications_controller.rb
@@ -5,9 +5,19 @@ module GrSync
       if params[:confirmation_url].present?
         ConfirmSubscriptionWorker.perform_async(params[:confirmation_url])
       else
-        NotificationWorker.perform_async(params)
+        NotificationWorker.perform_async(notification_params)
       end
       render nothing: true
+    end
+
+    private
+
+    def notification_params
+      # Don't use params because it will set action to 'create' because that's
+      # the controller action when what we really want is the action of the
+      # notification that global registry sent to us.
+      request.POST.slice(:action, :id, :client_integration_id, :triggered_by,
+                         :entity_type)
     end
   end
 end
--- spec/requests/gr_sync/notifications_spec.rb
+++ spec/requests/gr_sync/notifications_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-describe GrSync::NotificationsController do
+describe 'Global registry notifications', type: :request do
   context '#create' do
     it 'queues a subscription confirmation worker if confirmation url present' do
       allow(GrSync::ConfirmSubscriptionWorker).to receive(:perform_async)
 
-      post :create, confirmation_url: 'global-registry.org/confirm/abc'
+      post gr_sync_notifications_url, confirmation_url: 'global-registry.org/confirm/abc'
 
       expect(GrSync::ConfirmSubscriptionWorker).to have_received(:perform_async)
         .with('global-registry.org/confirm/abc')
@@ -15,7 +15,7 @@ describe GrSync::NotificationsController do
     it 'queues a notification worker if confirmation url not present' do
       allow(GrSync::NotificationWorker).to receive(:perform_async)
 
-      post :create, 'action' => 'updated', 'id' => '1f'
+      post gr_sync_notifications_url, action: 'updated', id: '1f'
 
       expect(GrSync::NotificationWorker).to have_received(:perform_async)
         .with('action' => 'updated', 'id' => '1f')
