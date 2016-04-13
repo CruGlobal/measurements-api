23ada2c1292d879aa3a4da4db3e511ac8002a256
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-04-13 16:02:42 -0400

    Point docker back to dockerhub

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-`/usr/local/bin/aws ecr get-login` &&
-docker build -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER .
+#`/usr/local/bin/aws ecr get-login` &&
+#docker build -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER .
+docker build -t cruglobal/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER .
