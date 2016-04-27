7c44c318dcabc7f0cff65fa5564b8b2658242b3a
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-04-27 16:18:02 -0400

    [#MAPI-142] Set default value for start_date

--- app/models/church/user_created_church.rb
+++ app/models/church/user_created_church.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class Church
   class UserCreatedChurch < ::Church
+    before_save :default_values
     after_create :write_audit
     validates :name, presence: { message: "Could not find required field: 'name'" }
     validates :ministry, presence: { message: "Could not find required field: 'ministry_id'" }
@@ -8,6 +9,10 @@ class Church
     def write_audit
       Audit.create(ministry_id: ministry_id, person_id: created_by_id, audit_type: :new_church,
                    message: "A new church created by #{created_by.full_name}: #{name}")
+    end
+
+    def default_values
+      self.start_date ||= Time.zone.today
     end
 
     authorize_values_for :ministry
