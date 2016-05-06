05727fc7b32c38824e5f177fa2157a4a03b4db3d
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-05-06 14:13:16 -0400

    Added re-run import comment

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 require 'import'
 
+# Console command to re-run import. This will delete all existing data.
+# def rerun_import
+#   Dir['./lib/*.rb'].each { |file| load file };
+#   ImportMappings::MAPPINGS.reverse.each do |mapping|
+#     klass = mapping.first
+#     klass.delete_all
+#   end
+#   ImportWorker.perform_async
+# end
+
 class ImportWorker
   include Sidekiq::Worker
   sidekiq_options unique: :until_and_while_executing, retry: false
