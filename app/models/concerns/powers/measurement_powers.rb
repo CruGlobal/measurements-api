37fa89c415bdbe0f021133ffacc6250068b96e24
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-05-11 15:13:14 -0400

    Update powers for Measurements

--- app/models/concerns/powers/measurement_powers.rb
+++ app/models/concerns/powers/measurement_powers.rb
@@ -6,7 +6,7 @@ module Powers
     included do
       # Power definitions
       power :historic_measurements do
-        inherited_assignment.try(:leader_role?)
+        assignment.try(:leader_role?) || inherited_assignment.try(:leader_role?)
       end
 
       power :measurements do
@@ -14,14 +14,14 @@ module Powers
       end
 
       power :showing_measurement do
-        true if inherited_assignment.try(:leader_role?)
+        assignment.try(:approved_role?) || inherited_assignment.try(:approved_role?)
       end
     end
 
     def measurement_levels
       levels = []
-      levels << :total if inherited_assignment.try(:approved_role?)
-      levels << :local if inherited_assignment.try(:leader_role?)
+      levels << :total if assignment.try(:approved_role?) || inherited_assignment.try(:leader_role?)
+      levels << :local if assignment.try(:leader_role?) || inherited_assignment.try(:leader_role?)
       levels << :person if assignment.present? && !assignment.blocked_role?
       levels
     end
