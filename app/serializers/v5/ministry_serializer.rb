053edecd06ad31a1bee013fb143b636608c81c5d
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-05-12 10:02:16 -0400

    Add id to ministry serializer

--- app/serializers/v5/ministry_serializer.rb
+++ app/serializers/v5/ministry_serializer.rb
@@ -3,5 +3,11 @@ module V5
   class MinistrySerializer < BaseMinistrySerializer
     has_many :team_members, serializer: TeamMemberSerializer
     has_many :children, key: :sub_ministries, serializer: MinistrySubMinistrySerializer
+
+    attributes :id
+
+    def id
+      object.gr_id
+    end
   end
 end
