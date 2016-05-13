8512b3d7cf2e5f8fa2f1ec1f1246f62ba94b8388
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-05-13 11:56:11 -0400

    Allow sys_assignments to update existing assignment on create

--- app/controllers/v5/systems_assignments_controller.rb
+++ app/controllers/v5/systems_assignments_controller.rb
@@ -3,6 +3,18 @@ module V5
   class SystemsAssignmentsController < V5::AssignmentsController
     include V5::BaseSystemsController
 
+    # Systems endpoint allows updating existing assignments on the create endpoint
+    def build_assignment
+      @assignment ||= ::Assignment::UserCreatedAssignment.new
+      @assignment.attributes = assignment_params
+      @assignment.save
+    rescue ActiveRecord::RecordNotUnique
+      found_assignment = ::Assignment.find_by(person_id: @assignment.person_id, ministry_id: @assignment.ministry_id)
+      return nil unless assignment.present?
+      @assignment = ::Assignment::UserUpdatedAssignment.new(found_assignment)
+      @assignment.update(role: assignment_params[:team_role])
+    end
+
     private
 
     def assignment_scope
