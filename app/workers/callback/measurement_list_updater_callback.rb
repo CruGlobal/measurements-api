c4fc4e6795c5193208ab2f66ae976632d5e5d596
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-06-07 17:04:50 -0400

    Implement a sidekiq batch callback instead of method in some class.

---
+++ app/workers/callback/measurement_list_updater_callback.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+module Callback
+  class MeasurementListUpdaterCallback
+    def on_success(_status, options)
+      # On success, queue up updating measurement totals
+      options[:json].each do |measurement|
+        GrSync::WithGrWorker.queue_call(options[:gr_client_params],
+                                        GrSync::MeasurementPush, :update_totals, measurement)
+      end
+    end
+  end
+end
