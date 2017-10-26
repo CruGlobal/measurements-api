ab200f306bf34f5504a951dd0e3d4c1baacba470
Josh Starcher <josh.starcher@gmail.com>
2017-10-26 13:38:19 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 docker build \
     --build-arg SIDEKIQ_CREDS=$SIDEKIQ_CREDS \
-    -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/measurements-api:$GIT_COMMIT-$BUILD_NUMBER .
+    -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/measurements-api:$ENVIRONMENT-$BUILD_NUMBER .
