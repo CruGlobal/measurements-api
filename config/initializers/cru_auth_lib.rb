ea9ced92351535186dfe0037f1f18c5621a09185
Justin Sabelko <justin.sabelko@cru.org>
2020-09-21 14:18:31 -0500

    One more redis var

--- config/initializers/cru_auth_lib.rb
+++ config/initializers/cru_auth_lib.rb
@@ -3,5 +3,5 @@
 require "cru_auth_lib"
 
 CruAuthLib.configure do |config|
-  config.redis_host = ENV.fetch("REDIS_PORT_6379_TCP_ADDR_SESSION")
+  config.redis_host = ENV.fetch("SESSION_REDIS_HOST")
 end
